e26766e3291cfa3de3f705813970437f9f74d42e
filwie <filip.wiechec@gmail.com>
2019-01-27 00:50:03 +0100

    Upgrade install script, remove ZSH_CUSTOM

--- install.sh
+++ install.sh
@@ -1,15 +1,43 @@
 #!/usr/bin/env zsh
 # vim: set ft=zsh sw=2 ts=2:
 
-local script_path="${0:h:A}"
+local log_file="/tmp/mini_dotfiles$(date)"
+
 local mini_dotfiles="${HOME}/.mini-dotfiles"
+local mini_dotfiles_repo_ssh="git@github.com:filwie/mini-dotfiles.git"
+local mini_dotfiles_repo_https="https://github.com/filwie/mini-dotfiles.git"
+
+local zsh_themes="${HOME}/.oh-my-zsh/themes"
+
+function run_log_cmd () {
+  # display command that is going to be ran and run it
+  # ARGS:
+  # 1 - cmd
+  local cmd="${1}"
+  echo -e "$(tput setaf 12)[$(date +'%H:%M:%S')] RUNNING: ${cmd}$(tput sgr0)"
+  eval "${cmd}"
+}
+
+function clone_mini_dotfiles () {
+  # clone mini dotfiles repo if its destination directory does not exist
+  # ARGS:
+  # 1 - repo url
+  local repo_url="${1}"
+  if ! [[ -d "${mini_dotfiles}" ]]; then
+    run_log_cmd "git clone ${repo_url} ${mini_dotfiles}"
+  fi
+}
 
 function link_mini_dotfiles () {
-  pushd "${script_path}" > /dev/null
+  pushd "${mini_dotfiles}" > /dev/null
   for dotfile in home/*; do
     local _src="$(realpath ${dotfile})"
     local _target="${HOME}/.$(basename ${dotfile})"
-    ln -s "${_src}" "${_target}"
+    if [[ "${_src}" =~ ".*.zsh-theme" ]]; then
+      _target="${zsh_themes}/$(basename ${_src})"
+    fi
+    [[ -e "${_target}" ]] && rm -r "${_target}"
+    run_log_cmd "ln -s ${_src} ${_target}"
   done
   popd > /dev/null
 }
@@ -35,5 +63,11 @@ function install_utilities () {
   fi
 }
 
-link_mini_dotfiles
-install_utilities
+function main () {
+  clone_mini_dotfiles "${mini_dotfiles_repo_ssh}" \
+    || clone_mini_dotfiles "${mini_dotfiles_repo_https}"
+  link_mini_dotfiles
+  install_utilities
+}
+
+main
