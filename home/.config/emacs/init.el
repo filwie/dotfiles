9e0feacdf2afba522559946d1f9cda9eb6769b9d
filwie <filip.wiechec@gmail.com>
2019-03-16 00:46:53 +0100

    [emacs] Enable mouse, change theme to gruvbox

--- home/.config/emacs/init.el
+++ home/.config/emacs/init.el
@@ -1,22 +1,46 @@
+;; Specify user directory based on env vars
+(if (not (getenv "XDG_CONFIG_HOME"))
+    (setenv "XDG_CONFIG_HOME" (concat (getenv "HOME") "/.config"))
+  )
+(setq user-emacs-directory (concat (getenv "XDG_CONFIG_HOME") "/emacs"))
+
+;; Packages list
+(setq package-to-install '(evil gruvbox-theme))
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
+
+;; Fetch packages list
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install packages
+(dolist (package package-to-install)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;; Mouse mode
+(unless window-system
+  (require 'mouse)
+  (xterm-mouse-mode t)
+  (when (string-equal system-type "darwin")
+    (global-set-key [mouse-4] (lambda () (interactive) (scroll-down 1)))
+    (global-set-key [mouse-5] (lambda () (interactive) (scroll-up 1)))
+    )
+  (defun track-mouse (e))
+  (setq mouse-sel-mode t)
+  )
+
+;; Evil mode
 (require 'evil)
 (evil-mode 1)
-; Use vi mode.  Remove this line for plain emacs.
-(setq term-setup-hook 'vip-mode)
 
-; Turn off binding of colon to eval
-(put 'eval-expression 'disabled nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (evil))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Theming
+;; Color scheme
+(load-theme 'gruvbox-light-soft t)
+;; Cursor shape in insert mode
+(unless (display-graphic-p)
+  (add-hook 'evil-insert-state-entry-hook (lambda () (send-string-to-terminal "\033[5 q")))
+  (add-hook 'evil-normal-state-entry-hook (lambda () (send-string-to-terminal "\033[0 q")))
+  )
