5e2eb4b1f11e9040b571426068166c243fc4d494
filwie <filip.wiechec@gmail.com>
2019-03-18 22:50:53 +0100

    Work on package list parser

--- packages/packages.py
+++ packages/packages.py
@@ -1,10 +1,18 @@
+import argparse
 import json
 from pathlib import Path
 
-packages_list = Path(__file__).parent / 'packages.json'
-packages = json.loads(packages_list.read_text())
 
-categories = packages.keys()
+def handle_args():
+    parser = argparse.ArgumentParser(description='List packages for given package manager.')
+    parser.add_argument('package_managers', metavar='package mangaer', type=str, nargs='+',
+                        help='package manager to list packages for')
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-    print(categories)
+    args = handle_args()
+    package_managers = args.package_managers
+    packages_list_file_path = Path(__file__).parent / 'packages.json'
+    packages = json.loads(packages_list.read_text())
+    print(package_managers)
