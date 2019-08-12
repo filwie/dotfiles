425705f210eef62c309efb7355a021c96c4b595a
filwie <filip.wiechec@gmail.com>
2019-08-12 16:08:35 +0200

    Make installation script actually do something

--- install.py
+++ install.py
@@ -16,11 +16,17 @@ XDG_CONFIG_HOME = Path(environ.get('XDG_CONFIG_HOME', f'{HOME}/.config'))
 REPO_HOME = script_path.parent / 'home'
 REPO_XDG_CONFIG_HOME = REPO_HOME / '.config'
 
+# TODO: refactor
+# TODO: add args (for replacing without prompting, for backing up etc)
+# TODO: add option for selecting dotfiles to link, do not link all of them
+
+
 if __name__ == '__main__':
     home_dotfiles = REPO_HOME.glob('*[!.config]*')
     xdg_dotfiles = REPO_XDG_CONFIG_HOME.glob('*')
 
     src_dest_map = {}
+    backed_up = []
 
     for dotfile in home_dotfiles:
         src_dest_map.update({dotfile: HOME / dotfile.relative_to(REPO_HOME)})
@@ -29,4 +35,16 @@ if __name__ == '__main__':
         src_dest_map.update({dotfile: XDG_CONFIG_HOME / dotfile.relative_to(REPO_XDG_CONFIG_HOME)})
 
     for src, dest in src_dest_map.items():
-        print(f'{src} -> {dest}')
+        if dest.samefile(src):
+            print(f'{dest} and {src} are the same file. Skipping...')
+            continue
+
+        if dest.exists():
+            print(f'{dest} already exists and is a different file than {src}.')
+            confirmation = input(f'Rename {dest} to {dest}.bak and continue? [Y/n] ')
+            if confirmation.lower() in ['', 'y', 'yes']:
+                dest.rename(f'{dest}.bak')
+            else:
+                continue
+
+        dest.symlink_to(src)
