df33c77397bb8ab458abf097a97d634ac2f66fe9
filwie <filip.wiechec@gmail.com>
2019-08-13 15:38:23 +0200

    Rewrite install script because of reasons.

--- install.py
+++ install.py
@@ -1,50 +1,108 @@
 #!/usr/bin/env python3
 
+import argparse
+from enum import Enum
 import sys
 from os import environ
 from pathlib import Path
+from typing import Dict, List
+import time
 
-try:
-    script_path = Path(__file__)
-    script_path = script_path.resolve()
-except NameError as err:
-    print('Unable to determine script path:', err, file=sys.stderr)
-    sys.exit(1)
 
-HOME = Path.home()
-XDG_CONFIG_HOME = Path(environ.get('XDG_CONFIG_HOME', f'{HOME}/.config'))
-REPO_HOME = script_path.parent / 'home'
-REPO_XDG_CONFIG_HOME = REPO_HOME / '.config'
+def handle_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Link dotfiles.')
+    parser.add_argument('-l', '--list', action='store_true', default=False,
+                        help='only list available dotfiles')
+    parser.add_argument('dotfiles', metavar='DOTFILE', nargs='*', type=str, default=['all'],
+                        help='one or more dotfiles to link (default: all)')
+    return parser.parse_args()
 
-# TODO: refactor
-# TODO: add args (for replacing without prompting, for backing up etc)
-# TODO: add option for selecting dotfiles to link, do not link all of them
+
+LinkResult = Enum('LinkResult', 'NOT_TOUCHED CREATED EXISTING BACKED_UP OVERWRITTEN IGNORED')
+
+
+class Dotfile:
+    str_template = '{: <20} | {: <20}'
+
+    def __init__(self, src: Path, dest: Path):
+        self.src = src
+        self.dest = dest
+        self.state = LinkResult.NOT_TOUCHED
+
+    @property
+    def name(self):
+        return self.src.stem.strip('.').lower()
+
+    @property
+    def rename_suffix(self):
+        return f'.bak.{int(time.time())}'
+
+    def link(self):
+        renamed = False
+        if self.dest.exists():
+            if self.dest.samefile(self.src):
+                self.state = LinkResult.EXISTING
+                return
+            else:
+                self.dest.rename(f'{self.dest}{self.rename_suffix}')
+                renamed = True
+        self.dest.symlink_to(self.src)
+        self.state = LinkResult.CREATED if not renamed else LinkResult.BACKED_UP
+
+    def __str__(self):
+        return Dotfile.str_template.format(self.name, self.state.name.lower(), self.src, self.dest)
+
+
+class DotfileMap:
+    exclude_glob = '*[!.config]*'
+
+    def __init__(self, src_dest_dirs: Dict[Path, Path]):
+        self.dotfiles = []
+        self.results = {}
+        for src_dir, dest_dir in src_dest_dirs.items():
+            self._register(src_dir, dest_dir)
+
+    def _register(self, src_dir: Path, dest_dir: Path):
+        for src in src_dir.glob(DotfileMap.exclude_glob):
+            self.dotfiles.append(Dotfile(src, dest_dir / src.relative_to(src_dir)))
+
+    def link_one(self, dotfile: Dotfile):
+        self.results.update({dotfile: dotfile.link()})
+
+    def link_some(self, dotfiles: List[Dotfile]):
+        for dotfile in dotfiles:
+            self.link_one(dotfile)
+
+    def link_all(self): self.link_some(self.dotfiles)
+
+    def __str__(self):
+        return '\n'.join(str(d) for d in self.dotfiles)
 
 
 if __name__ == '__main__':
-    home_dotfiles = REPO_HOME.glob('*[!.config]*')
-    xdg_dotfiles = REPO_XDG_CONFIG_HOME.glob('*')
+    try:
+        repo_root = Path(__file__).parent.resolve()
+    except (NameError, FileNotFoundError) as err:
+        print('Unable to determine repo root.', err, file=sys.stderr)
+        sys.exit(1)
 
-    src_dest_map = {}
-    backed_up = []
+    home = Path.home()
+    repo_home = repo_root / 'home'
 
-    for dotfile in home_dotfiles:
-        src_dest_map.update({dotfile: HOME / dotfile.relative_to(REPO_HOME)})
+    xdg_config_home = Path(environ.get('XDG_CONFIG_HOME', f'{home}/.config'))
+    xdg_config_home.mkdir(exist_ok=True)
+    repo_xdg_config_home = repo_home / '.config'
 
-    for dotfile in xdg_dotfiles:
-        src_dest_map.update({dotfile: XDG_CONFIG_HOME / dotfile.relative_to(REPO_XDG_CONFIG_HOME)})
+    args = handle_args()
 
-    for src, dest in src_dest_map.items():
-        if dest.samefile(src):
-            print(f'{dest} and {src} are the same file. Skipping...')
-            continue
+    dm = DotfileMap({
+        repo_home: home,
+        repo_xdg_config_home: xdg_config_home
+    })
 
-        if dest.exists():
-            print(f'{dest} already exists and is a different file than {src}.')
-            confirmation = input(f'Rename {dest} to {dest}.bak and continue? [Y/n] ')
-            if confirmation.lower() in ['', 'y', 'yes']:
-                dest.rename(f'{dest}.bak')
-            else:
-                continue
-
-        dest.symlink_to(src)
+    if args.list:
+        print('Available dotfiles: {}'.format(' '.join(d.name for d in dm.dotfiles)))
+    elif 'all' in args.dotfiles:
+        dm.link_all()
+    else:
+        dm.link_some(filter(lambda d: d.name in args.dotfiles, dm.dotfiles))
