6601fa7d0fb5f63fe14145068ef626d69375d584
filwie <filip.wiechec@gmail.com>
2019-10-08 23:12:47 +0200

    Update Fish theme

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -32,8 +32,104 @@ else
 end
 # /vars }}}
 
+# os glyph, os detection {{{
+
+## known_oses {{{
+set -g known_oses \
+    arch \
+    bsd \
+    centos \
+    debian \
+    fedora \
+    linux \
+    mac \
+    manjaro \
+    opensuse \
+    raspbian \
+    redhat \
+    ubuntu \
+    unknown
+## /known_oses }}}
+
+## known_oses_colors {{{
+set -g known_oses_colors \
+    0F94D2 \
+    770000 \
+    EFA724 \
+    C70036 \
+    00457E \
+    000000  \
+    FFFFFF \
+    35BF5C \
+    73BA25 \
+    C31C4A \
+    EE0000 \
+    E95420 \
+    AEAEAE
+## /known_oses_colors }}}
+
+## known_oses_glyphs {{{
+set -g known_oses_glyphs \
+    (printf '%s%s%s' (set_color 0F94D2 ) ' ' (set_color normal))\
+    (printf '%s%s%s' (set_color 770000 ) ' ' (set_color normal)) \
+    (printf '%s%s%s' (set_color EFA724 ) ' ' (set_color normal)) \
+    (printf '%s%s%s' (set_color C70036 ) ' ' (set_color normal)) \
+    (printf '%s%s%s' (set_color 00457E ) ' ' (set_color normal)) \
+    (printf '%s%s%s' (set_color 000000 ) ' ' (set_color normal)) \
+    (printf '%s%s%s' (set_color FFFFFF ) ' ' (set_color normal)) \
+    (printf '%s%s%s' (set_color 35BF5C ) ' ' (set_color normal)) \
+    (printf '%s%s%s' (set_color 73BA25 ) ' ' (set_color normal)) \
+    (printf '%s%s%s' (set_color C31C4A ) ' ' (set_color normal)) \
+    (printf '%s%s%s' (set_color EE0000 ) ' ' (set_color normal)) \
+    (printf '%s%s%s' (set_color E95420 ) ' ' (set_color normal)) \
+    (printf '%s%s%s' (set_color AEAEAE ) '' (set_color normal))
+## /known_oses_glyphs }}}
+
+set -g known_os_regex (printf '%s' (string join '|' $known_oses))
+
+function _distro
+    if test -f /etc/os-release
+        string lower (string match -ir $known_os_regex (head -n1 /etc/os-release)); or printf 'linux'
+    end
+end
+
+function _os -d "Detect os type (and Linux distro if linux)"  # {{{
+    if not set -q THEME_OS
+        switch (uname)
+        case Linux
+            set -gx THEME_OS (_distro)
+        case Darwin
+            set -gx THEME_OS mac
+        case '*'
+            set -gx THEME_OS other
+        end
+    end
+    printf $THEME_OS
+end  # }}}
+
+function _os_glyph
+    if not set -q THEME_OS_GLYPH
+        set index (contains -i (_os) $known_oses)
+        set -gx THEME_OS_GLYPH $known_oses_glyphs[$index]
+    end
+    printf $THEME_OS_GLYPH
+end
+
+function _os_color
+    if not set -q THEME_OS_COLOR
+        set index (contains -i (_os) $known_oses)
+        set -gx THEME_OS_COLOR $known_oses_colors[$index]
+    end
+    printf $THEME_OS_COLOR
+end
+# /os glyph, os detection }}}
+
 function _prompt_end  # {{{
-    set_color blue
+    if set -q THEME_ENABLE_GLYPHS
+        set_color (_os_color)
+    else
+        set_color blue
+    end
     test $RC -eq 0; or set_color red
     set_color --bold
     if test (id -u) -eq 0; printf "#"
@@ -54,91 +150,38 @@ end  # }}}
 
 function _git_remote_glyph  # {{{
     set -q THEME_ENABLE_GLYPHS; or return
-    if command git rev-parse --is-inside-work-tree 2>&1
+    if command git rev-parse --is-inside-work-tree > /dev/null 2>&1
         switch (command git remote get-url origin)
         case '*github.com*'
-            set -g dir_glyph ' '
+            set dir_glyph ' '
+            set dir_glyph_color FFFFFF
         case '*gitlab.com*'
-            set -g dir_glyph ' '
+            set dir_glyph ' '
+            set dir_glyph_color E24329
         case '*bitbucket.com*'
-            set -g dir_glyph ' '
+            set dir_glyph ' '
+            set dir_glyph_color 2684FF
         case '*'
-            set -g dir_glyph ' '
+            set dir_glyph ' '
+            set dir_glyph_color F05033
         end
     end
-    printf '%s' $dir_glyph
+    printf '%s%s%s' (set_color $dir_glyph_color) $dir_glyph (set_color normal)
 end  # }}}
-
-# os glyph, os detection {{{
-
-## known_oses {{{
-set -g known_oses \
-    arch \
-    bsd \
-    centos \
-    debian \
-    fedora \
-    linux \
-    mac \
-    manjaro \
-    opensuse \
-    raspbian \
-    redhat \
-    ubuntu \
-    unknown
-## /known_oses }}}
-
-## known_oses_glyphs {{{
-set -g known_oses_glyphs \
-    (printf '%s%s%s' (set_color 0F94D2 ) ' ' (set_color normal))\
-    (printf '%s%s%s' (set_color 770000 ) ' ' (set_color normal)) \
-    (printf '%s%s%s' (set_color EFA724 ) ' ' (set_color normal)) \
-    (printf '%s%s%s' (set_color C70036 ) ' ' (set_color normal)) \
-    (printf '%s%s%s' (set_color 00457E ) ' ' (set_color normal)) \
-    (printf '%s%s%s' (set_color black ) ' ' (set_color normal)) \
-    (printf '%s%s%s' (set_color FFFFFF ) ' ' (set_color normal)) \
-    (printf '%s%s%s' (set_color 35BF5C ) ' ' (set_color normal)) \
-    (printf '%s%s%s' (set_color 73BA25 ) ' ' (set_color normal)) \
-    (printf '%s%s%s' (set_color C31C4A ) ' ' (set_color normal)) \
-    (printf '%s%s%s' (set_color EE0000 ) ' ' (set_color normal)) \
-    (printf '%s%s%s' (set_color E95420 ) ' ' (set_color normal)) \
-    (printf '%s%s%s' (set_color AEAEAE ) '' (set_color normal))
-## /known_oses_glyphs }}}
-
-set -g known_os_regex (printf '%s' (string join '|' $known_oses))
-
-function _distro
-    if test -f /etc/os-release
-        string lower (string match -ir $known_os_regex (head -n1 /etc/os-release)); or printf 'linux'
-    end
-end
-
-function _os -d "Detect os type (and Linux distro if linux)"  # {{{
-    switch (uname)
-    case Linux
-        set _os (_distro)
-    case Darwin
-        set _os mac
-    case '*'
-        set _os other
-    end
-    printf '%s' $_os
-end  # }}}
-
-function _os_glyph
-    set index (contains -i (_os) $known_oses)
-    printf $known_oses_glyphs[$index]
-end
-# /os glyph, os detection }}}
 
 function _path  # {{{
-    not set -q THEME_ENABLE_GLYPHS; or printf '  '
     printf '%s%s%s' (set_color --bold) (prompt_pwd) (set_color normal)
 end  # }}}
 
 function _jobs  # {{{
     test $NJOBS -gt 0; or return
     printf '%sj:%s%s' (set_color --bold brblue) $NJOBS (set_color normal)
+end  # }}}
+
+function _running_docker_containers  # {{{
+    command -v docker > /dev/null; or return
+    set -l cn (docker ps -q | wc -l)
+    printf '%s%s' $cn
 end  # }}}
 
 # language versions {{{
@@ -163,6 +206,7 @@ function fish_prompt --description 'Write out the prompt'
     set -g RC $status
     set -g NJOBS (jobs -c | wc -l | awk '{print $1}')
 
+    not set -q THEME_ENABLE_GLYPHS; or segment (_os_glyph)
     segment (_jobs)
     segment (_path)
     segment (_prompt_end)
@@ -170,14 +214,16 @@ end
 
 function fish_right_prompt
     segment (_err_code)
+    segment (_git_remote_glyph)
     segment (__fish_git_prompt "%s")
 
     not set -q THEME_ENABLE_GLYPHS; or _incolor '7FD5EA' ' '
     segment (_incolor brblue (_go_version))
 
-    not set -q THEME_ENABLE_GLYPHS; or printf '%s ' (set_color 3E7BAC)
+    not set -q THEME_ENABLE_GLYPHS; or _incolor '3E7BAC' ' '
     segment (_incolor blue (_python_version))
+    segment (_incolor yellow (_python_venv))
 
-    set_color --bold yellow; segment (_python_venv)
-    set_color normal
+    not set -q THEME_ENABLE_GLYPHS; or _incolor '099CEC' '  '
+    segment (_incolor blue (_running_docker_containers))
 end
