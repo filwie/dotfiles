e407473f924704a25ccf47cddbfebdb5d3697578
filwie <filip.wiechec@gmail.com>
2019-10-14 21:46:30 +0200

    Set PATH in a more elegant manner, check for duplicates

--- home/.config/fish/path.fish
+++ home/.config/fish/path.fish
@@ -7,11 +7,11 @@ set -l add_to_path_if_exists \
 
 switch (uname)
 case Darwin
-    set -Ux fish_user_paths $HOME/Library/Python/**/bin/ $fish_user_paths
+    set --append PATH $HOME/Library/Python/**/bin/
 end
 
 for bin_dir in $add_to_path_if_exists
-    if test -d
-        set -Ux fish_user_paths $bin_dir $fish_user_paths
+    if test -d $bin_dir && not contains $bin_dir $PATH
+        set --append PATH $bin_dir
     end
 end
