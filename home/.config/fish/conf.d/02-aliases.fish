941216172deb02bfdc78c663919104114694b01c
filwie <filip.wiechec@gmail.com>
2020-02-13 21:17:51 +0100

    Make Fish use ansi colors in theme

--- home/.config/fish/conf.d/02-aliases.fish
+++ home/.config/fish/conf.d/02-aliases.fish
@@ -2,3 +2,4 @@
 alias tmux 'tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf'
 alias ssh 'env TERM=xterm-256color ssh'
 alias vagrant 'env TERM=xterm-256color vagrant'
+alias vim nvim
---
+++ home/.config/fish/conf.d/20-colors.fish
@@ -0,0 +1,35 @@
+set -U fish_color_normal              normal
+set -U fish_color_command             cyan
+set -U fish_color_param               yellow
+set -U fish_color_quote               green --italics
+
+set -U fish_color_operator            brred --bold
+set -U fish_color_redirection         brred
+set -U fish_color_end                 brred --bold
+
+set -U fish_color_error               red --underline
+
+set -U fish_color_autosuggestion      brblack
+set -U fish_color_valid_path          --underline
+
+set -U fish_color_cancel              -r
+set -U fish_color_comment             cyan
+
+set -U fish_color_cwd                 green
+set -U fish_color_cwd_root            red
+
+set -U fish_color_escape              brcyan
+set -U fish_color_history_current     --bold
+set -U fish_color_host                normal
+set -U fish_color_match               --background=brblue
+
+
+set -U fish_color_search_match        yellow --background=black
+set -U fish_color_selection           white --bold --background=black
+set -U fish_color_status              red
+set -U fish_color_user                brgreen
+
+set -U fish_pager_color_completion    normal
+set -U fish_pager_color_description   yellow
+set -U fish_pager_color_prefix        white --bold --underline
+set -U fish_pager_color_progress      brwhite --background=cyan
