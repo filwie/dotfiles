b79e9205d549893536b90e06f1854c84282c67a5
filwie <filip.wiechec@gmail.com>
2020-11-02 20:51:05 +0100

    Finish installation script

--- install.sh
+++ install.sh
@@ -1,18 +1,86 @@
 #!/usr/bin/env bash
-set -eu
+set -euo pipefail
 
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"${HOME}/.config"}"
+XDG_BIN_HOME="${XDG_BIN_HOME:-"${HOME}/.local/bin"}"
 XDG_DATA_HOME="${XDG_DATA_HOME:-"${HOME}/.local/share"}"
 
 REPO_ROOT="$(cd "$(dirname "$(realpath "${0}")")" || exit; git rev-parse --show-toplevel)"
-source "$REPO_ROOT/scripts/lib.sh"
+CONFIGS_DIR="${REPO_ROOT}/home/.config"
+SCRIPTS_DIR="${REPO_ROOT}/home/.local/bin"
 
-declare -A LINK_MAP=(
-    ["${REPO_ROOT}/home/.local/bin/*"]="${HOME}/.local/bin/"
-    ["${REPO_ROOT}/home/.config/*"]="${HOME}/.local/config/"
-)
+cd "${CONFIGS_DIR}"
+CONFIGS=(*)
+cd - > /dev/null
+cd "${SCRIPTS_DIR}"
+SCRIPTS=(*)
+cd - > /dev/null
 
-# lib.display_as_table LINK_MAP
-lib.border_top "${1}"
-lib.border_mid "${1}"
-lib.border_bot "${1}"
+function list_dotfiles {
+    echo "Configs: ${CONFIGS[*]}"
+    echo "Scripts: ${SCRIPTS[*]}"
+}
+
+function help {
+    echo "Usage: ${0} [option] [config|script]"
+    echo
+    echo "  -a, --all               link all configs and scripts"
+    echo "  -l, --list              list available configs and scripts"
+    echo "  -c, --configs {CONFIG}  link specified config(s) only"
+    echo "  -s, --scripts {SCRIPT}  link specified script(s) only"
+    echo "  --all-configs           link all configs"
+    echo "  --all-scripts           link all scripts"
+    echo "  -l, --list  list available configs and scripts"
+    exit
+}
+
+function link_one {
+    local src dest
+    src="$(realpath "${_src_dir:-${CONFIGS_DIR}}/${1:?file or directory in ${CONFIGS_DIR}}")"
+    dest="$(realpath "${2:-${_dest:-${XDG_CONFIG_HOME}}}")"
+
+    if ! [ -e "${src}" ]; then
+        echo "${src} does not exist"
+        return 1
+    fi
+    printf "${src} -> ${dest}/$(basename ${src}): "
+    if [[ "$(ln -s "${src}" "${dest}" 2>&1 >/dev/null)" =~ exists ]]; then
+        echo "destination exists. Skipping..."
+    else
+        echo "linked."
+    fi
+}
+
+function link_many {
+    for src in "${@}"; do
+        link_one "${src}"
+    done
+}
+
+case "${1:---help}" in
+    -a|--all)
+        link_many "${CONFIGS[@]}"
+        _src_dir="${SCRIPTS_DIR}" _dest="${XDG_BIN_HOME}" link_many "${SCRIPTS[@]}"
+        ;;
+    -l|--list)
+        list_dotfiles
+        ;;
+    -c|--configs)
+        shift
+        link_many "${@}"
+        ;;
+    --all-configs)
+        link_many "${CONFIGS[@]}"
+        ;;
+    -s|--scripts)
+        shift
+        _src_dir="${SCRIPTS_DIR}" link_many "${@}"
+        ;;
+    --all-scripts)
+        _src_dir="${SCRIPTS_DIR}" _dest="${XDG_BIN_HOME}" link_many "${SCRIPTS[@]}"
+        ;;
+    *)
+        help
+        ;;
+esac
+
