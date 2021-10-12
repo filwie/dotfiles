4e78e7354689f082882b4d66146a8bd8408ba843
filwie <filip.wiechec@gmail.com>
2021-10-12 23:53:18 +0200

    Add wezterm config

---
+++ home/.config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm';
+-- cursor theme: xfconf-query --channel xsettings --property /Gtk/CursorThemeName --set Adwaita
+return {
+  color_scheme = "Gruvbox Dark",
+  hide_tab_bar_if_only_one_tab = true,
+  font = wezterm.font("FantasqueSansMono Nerd Font"),
+  font_size = 14.0,
+  set_environment_variables = {
+      TMUX_ALWAYS = "true",
+  },
+}
+
