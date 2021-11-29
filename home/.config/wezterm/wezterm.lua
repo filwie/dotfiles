0f15d9f55f8378ae338f77521486868015161506
filwie <filip.wiechec@gmail.com>
2021-11-30 00:15:16 +0100

    Add var for tmux status line

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -3,11 +3,12 @@ local wezterm = require 'wezterm';
 return {
   color_scheme = "Gruvbox Dark",
   hide_tab_bar_if_only_one_tab = true,
-  font = wezterm.font("FantasqueSansMono Nerd Font"),
-  font_size = 14.0,
+  font = wezterm.font("Fantasque Sans Mono"),
+  font_size = 13.0,
   set_environment_variables = {
-      TMUX_ALWAYS = "true",
       THEME_ENABLE_GLYPHS = "1",
+      TMUX_ALWAYS = "true",
+      LINEGLYPH = ""
   },
 }
 
