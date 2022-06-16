df09963d02cee88d788a6b92e7d1659414593371
filwie <filip.wiechec@gmail.com>
2022-06-16 21:48:34 +0200

    Retab wezterm conf

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -1,17 +1,18 @@
 local wezterm = require 'wezterm';
 -- cursor theme: xfconf-query --channel xsettings --property /Gtk/CursorThemeName --set Adwaita
 return {
-  color_scheme = "Gruvbox Dark",
-  hide_tab_bar_if_only_one_tab = true,
-  font = wezterm.font("Fantasque Sans Mono"),
-  font_size = 13.0,
-  set_environment_variables = {
-      THEME_ENABLE_GLYPHS = "1",
-      TMUX_ALWAYS = "true",
-      POWERLINE_TR = "",
-      POWERLINE_TL = "",
-      POWERLINE_BR = "",
-      POWERLINE_BL = "",
-  },
+    color_scheme = "Gruvbox Dark",
+    hide_tab_bar_if_only_one_tab = true,
+    font = wezterm.font("Fantasque Sans Mono"),
+    font_size = 13.0,
+    window_background_opacity = 1.0,
+    set_environment_variables = {
+        THEME_ENABLE_GLYPHS = "1",
+        TMUX_ALWAYS = "true",
+        POWERLINE_TR = "",
+        POWERLINE_TL = "",
+        POWERLINE_BR = "",
+        POWERLINE_BL = "",
+    },
 }
 
