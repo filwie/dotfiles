0f92155033ff11111362be17d831889247f78176
filwie <filip.wiechec@gmail.com>
2024-07-15 22:50:23 +0200

    Add theme mode detection

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+local theme_mode = os.getenv("THEME_MODE") or "dark"
+if theme_mode == "light" then
+  vim.o.background = "light"
+end
 require("config.lazy")
